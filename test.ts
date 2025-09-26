e47827d5a3f72c86c6a2d0505410d7a0f897caa6
Pattarapon0 <pattarapons.29.05@gmail.com>
2025-09-26 17:19:58 +0700

    refactor test.tsx

--- test.ts
+++ test.ts
@@ -4,7 +4,12 @@ import * as assert from "uvu/assert";
 import { merge } from "./mergeSort";
 
 test("merge sorts correctly", () => {
-  assert.equal(merge([1, 2, 3, 5, 20], [20,4, 2, 0], [6, 8,10,11]), [0, 1, 2, 2, 3, 4, 5, 6, 8, 10, 11, 20, 20]);
+  let collection_1 = [1, 2, 3, 5, 20];
+  let collection_2 = [20, 4, 2, 0];
+  let collection_3 = [6, 8, 10, 11];
+  let expected = [0, 1, 2, 2, 3, 4, 5, 6, 8, 10, 11, 20, 20];
+  let result = merge(collection_1, collection_2, collection_3);
+  assert.equal(result, expected);
 });
 
 
